2f241258a850bf2a01e39f60c5eb0c887492f978
E. A. Graham Jr. <10370165+EAGrahamJr@users.noreply.github.com>
2024-07-14 17:19:42 -0700

    Apparently fixing broken black vs PyCharm formatting
    And let's not talk about that copyright thing (what???)

--- ha-minimqtt/NumberEntity.py
+++ ha-minimqtt/NumberEntity.py
@@ -1,47 +1,25 @@
-#  MIT License
+# MIT License
 #
-#  Copyright (c) 2024 E. A. (Ed) Graham, Jr.
+# Copyright (c) 2024 E. A. (Ed) Graham, Jr.
 #
-#  Permission is hereby granted, free of charge, to any person obtaining a copy
-#  of this software and associated documentation files (the "Software"), to deal
-#  in the Software without restriction, including without limitation the rights
-#  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-#  copies of the Software, and to permit persons to whom the Software is
-#  furnished to do so, subject to the following conditions:
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
 #
-#  The above copyright notice and this permission notice shall be included in all
-#  copies or substantial portions of the Software.
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
 #
-#  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-#  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-#  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-#  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-#  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-#  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-#  SOFTWARE.
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 
-#  MIT License
-#
-#
-#  Permission is hereby granted, free of charge, to any person obtaining a copy
-#  of this software and associated documentation files (the "Software"), to deal
-#  in the Software without restriction, including without limitation the rights
-#  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-#  copies of the Software, and to permit persons to whom the Software is
-#  furnished to do so, subject to the following conditions:
-#
-#
-#  MIT License
-#
-#
-#  Permission is hereby granted, free of charge, to any person obtaining a copy
-#  of this software and associated documentation files (the "Software"), to deal
-#  in the Software without restriction, including without limitation the rights
-#  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-#  copies of the Software, and to permit persons to whom the Software is
-#  furnished to do so, subject to the following conditions:
-#
-#
 from enum import Enum
 
 from base import CommandEntity, DeviceIdentifier, DeviceClass
@@ -132,17 +110,17 @@ class NumberEntity(CommandEntity):
     """
 
     def __init__(
-            self,
-            unique_id: str,
-            name: str,
-            device: DeviceIdentifier,
-            handler: NumberHandler,
-            device_class: NumericDevice = NumericDevice.NONE,
-            minimum: int = 1,
-            maximum: int = 100,
-            step: float = 1.0,
-            mode: DisplayMode = DisplayMode.AUTO,
-            unit_of_measurement: str = None,
+        self,
+        unique_id: str,
+        name: str,
+        device: DeviceIdentifier,
+        handler: NumberHandler,
+        device_class: NumericDevice = NumericDevice.NONE,
+        minimum: int = 1,
+        maximum: int = 100,
+        step: float = 1.0,
+        mode: DisplayMode = DisplayMode.AUTO,
+        unit_of_measurement: str = None,
     ):
         super().__init__("number", unique_id, name, device)
         self._class = device_class
--- ha-minimqtt/sensors.py
+++ ha-minimqtt/sensors.py
@@ -1,36 +1,25 @@
-#  MIT License
+# MIT License
 #
-#  Copyright (c) 2024 E. A. (Ed) Graham, Jr.
+# Copyright (c) 2024 E. A. (Ed) Graham, Jr.
 #
-#  Permission is hereby granted, free of charge, to any person obtaining a copy
-#  of this software and associated documentation files (the "Software"), to deal
-#  in the Software without restriction, including without limitation the rights
-#  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-#  copies of the Software, and to permit persons to whom the Software is
-#  furnished to do so, subject to the following conditions:
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
 #
-#  The above copyright notice and this permission notice shall be included in all
-#  copies or substantial portions of the Software.
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
 #
-#  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-#  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-#  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-#  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-#  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-#  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-#  SOFTWARE.
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 
-#  MIT License
-#
-#
-#  Permission is hereby granted, free of charge, to any person obtaining a copy
-#  of this software and associated documentation files (the "Software"), to deal
-#  in the Software without restriction, including without limitation the rights
-#  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-#  copies of the Software, and to permit persons to whom the Software is
-#  furnished to do so, subject to the following conditions:
-#
-#
 from datetime import timedelta
 from enum import Enum
 
@@ -39,13 +28,13 @@ from base import AbstractBaseEntity, DeviceIdentifier, DeviceClass
 
 class AbstractSensor(AbstractBaseEntity):
     def __init__(
-            self,
-            component: str,
-            unique_id: str,
-            name: str,
-            device: DeviceIdentifier,
-            expires: timedelta = None,
-            device_class: DeviceClass = None,
+        self,
+        component: str,
+        unique_id: str,
+        name: str,
+        device: DeviceIdentifier,
+        expires: timedelta = None,
+        device_class: DeviceClass = None,
     ):
         super().__init__(component, unique_id, name, device)
         self._expires = expires
@@ -113,13 +102,13 @@ class BinaryDevice(DeviceClass):
 
 class BinarySensor(AbstractSensor):
     def __init__(
-            self,
-            unique_id: str,
-            name: str,
-            device: DeviceIdentifier,
-            expires: timedelta = timedelta(0),
-            device_class: BinaryDevice = BinaryDevice.NONE,
-            off_delay: timedelta = timedelta(0),
+        self,
+        unique_id: str,
+        name: str,
+        device: DeviceIdentifier,
+        expires: timedelta = timedelta(0),
+        device_class: BinaryDevice = BinaryDevice.NONE,
+        off_delay: timedelta = timedelta(0),
     ):
         super().__init__(
             "binary_sensor", unique_id, name, device, expires, device_class
@@ -209,15 +198,15 @@ class AnalogDevice(DeviceClass):
 class AnalogSensor(AbstractSensor):
 
     def __init__(
-            self,
-            unique_id: str,
-            name: str,
-            device: DeviceIdentifier,
-            expires: timedelta = None,
-            device_class: AnalogDevice = AnalogDevice.NONE,
-            state_class: StateClass = StateClass.NONE,
-            unit_of_measurement: str = None,
-            suggested_precision: int = None,
+        self,
+        unique_id: str,
+        name: str,
+        device: DeviceIdentifier,
+        expires: timedelta = None,
+        device_class: AnalogDevice = AnalogDevice.NONE,
+        state_class: StateClass = StateClass.NONE,
+        unit_of_measurement: str = None,
+        suggested_precision: int = None,
     ):
         super().__init__("sensor", unique_id, name, device, expires, device_class)
 
